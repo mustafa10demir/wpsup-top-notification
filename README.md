985d325ed152b98114933f05282e53c353cde2d1
mustafademir <mustafa10demir@gmail.com>
2023-07-31 21:45:35 +0300

    Create Plugin file and settings

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# wpsup-top-notification
+# WpSup Top Notification
 Enhance your website with a handy notification bar using our WordPress plugin! Display important announcements or promotions at the top of your site with ease.
---
+++ wpsup-top-notification.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:       Top Notification
+ * Description:       Enhance your website with a handy notification bar using our WordPress plugin! Display important announcements or promotions at the top of your site with ease.
+ * Plugin URI:        https://wpsup.co/plugins/notification
+ * Version:           1.0.0
+ * Requires at least: 5.7
+ * Requires PHP:      7.4
+ * Author:            WpSup
+ * Author URI:        https://wpsup.co/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       wpsup-header-notification
+ * Domain Path:       /languages/
+ *
+ * @package wp-sup-top-notification
+ */
