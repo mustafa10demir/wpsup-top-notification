cce6f5142547a8d566143da3872f37a158bd03e4
mustafademir <mustafa10demir@gmail.com>
2023-07-31 21:49:32 +0300

    SPL autoload and defined settings

--- wpsup-top-notification.php
+++ wpsup-top-notification.php
@@ -13,5 +13,45 @@
  * Text Domain:       wpsup-header-notification
  * Domain Path:       /languages/
  *
- * @package wp-sup-top-notification
+ * @package wpsup-top-notification
  */
+
+namespace HeaderNotification;
+
+defined( 'ABSPATH' ) || exit;
+
+define( 'WP_SUP_TOP_NOT_PLUGIN_DIR', untrailingslashit( dirname( __FILE__ ) ) );
+define( 'WP_SUP_TOP_NOT_PLUGIN_DIR_URL', plugin_dir_url( __FILE__ ) );
+define( 'WP_SUP_TOP_NOT_PLUGIN_DOMAIN', 'wpsup-top-notification' );
+define( 'WP_SUP_TOP_NOT_PLUGIN_VERSION', '1.0.0' );
+
+/**
+ * PSR-4-ish autoloading
+ *
+ * @since 2.6
+ */
+spl_autoload_register(
+	function ( $class ) {
+		// project-specific namespace prefix.
+		$prefix = 'TopNotification\\';
+
+		// base directory for the namespace prefix.
+		$base_dir = __DIR__ . '/includes/classes/';
+
+		// does the class use the namespace prefix?
+		$len = strlen( $prefix );
+
+		if ( strncmp( $prefix, $class, $len ) !== 0 ) {
+			return;
+		}
+
+		$relative_class = substr( $class, $len );
+
+		$file = $base_dir . str_replace( '\\', '/', $relative_class ) . '.php';
+
+		// if the file exists, require it.
+		if ( file_exists( $file ) ) {
+			require $file;
+		}
+	}
+);
